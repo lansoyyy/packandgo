264acbe38a5036e12aa7cd591f8653caa0981a06
Lans12345 <olanalans12345@gmail.com>
2023-06-15 06:55:40 +0800

    added views on proj structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:packandgo/screens/home_screen.dart';
+import 'package:packandgo/views/web/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
