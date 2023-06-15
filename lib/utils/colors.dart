c661ac7c7715bfe5cfcaebbac32c642d1015d326
Lans12345 <olanalans12345@gmail.com>
2023-06-15 12:47:59 +0800

    added login screen

---
+++ lib/utils/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final primary = Colors.purple[900];
--- lib/views/web/screens/auth/login_screen.dart
+++ lib/views/web/screens/auth/login_screen.dart
@@ -1,10 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:packandgo/widgets/button_widget.dart';
+import 'package:packandgo/widgets/text_widget.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 65,
+                  ),
+                  const Expanded(child: SizedBox()),
+                  TextButton(
+                    onPressed: () {},
+                    child: TextRegular(
+                      text: 'Login',
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  ButtonWidget(
+                    height: 45,
+                    width: 125,
+                    label: 'Signup',
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextBold(
+                text: 'Moving\nis what\nwe love',
+                fontSize: 68,
+                color: Colors.white,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextRegular(
+                text:
+                    'In Pack & Go, we make sure\nyour items is delivered safely.',
+                fontSize: 28,
+                color: Colors.white,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ButtonWidget(
+                color: Colors.blue,
+                height: 45,
+                width: 150,
+                label: 'Book now',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
