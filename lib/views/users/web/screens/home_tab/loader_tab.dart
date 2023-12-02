1804839f3d962c8795c885deb762e373497fad7f
Lans12345 <olanalans12345@gmail.com>
2023-12-02 09:21:39 +0800

    fixed additional revisions

--- lib/views/users/web/screens/home_tab/loader_tab.dart
+++ lib/views/users/web/screens/home_tab/loader_tab.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:packandgo/utils/colors.dart';
 import 'package:packandgo/views/users/web/screens/home_screen.dart';
 
 import 'package:packandgo/widgets/button_widget.dart';
@@ -19,6 +18,7 @@ class _LoaderTabState extends State<LoaderTab> {
 
   int dropValue = 0;
   int dropValue1 = 0;
+  int dropValue2 = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -38,9 +38,9 @@ class _LoaderTabState extends State<LoaderTab> {
             height: 20,
           ),
           ButtonWidget(
-            textColor: Colors.black,
-            color: Colors.white,
-            width: 500,
+            textColor: Colors.white,
+            color: Colors.green,
+            width: 150,
             radius: 5,
             label: 'Yes',
             onPressed: () {
@@ -55,9 +55,9 @@ class _LoaderTabState extends State<LoaderTab> {
             height: 20,
           ),
           ButtonWidget(
-            color: Colors.white,
-            textColor: Colors.red,
-            width: 500,
+            textColor: Colors.white,
+            color: Colors.red,
+            width: 150,
             radius: 5,
             label: 'No',
             onPressed: () {
@@ -84,9 +84,9 @@ class _LoaderTabState extends State<LoaderTab> {
             height: 20,
           ),
           ButtonWidget(
-            textColor: Colors.black,
-            color: Colors.white,
-            width: 500,
+            textColor: Colors.white,
+            color: Colors.green,
+            width: 150,
             radius: 5,
             label: 'Yes',
             onPressed: () {
@@ -100,9 +100,9 @@ class _LoaderTabState extends State<LoaderTab> {
             height: 20,
           ),
           ButtonWidget(
-            color: Colors.white,
-            textColor: Colors.red,
-            width: 500,
+            textColor: Colors.white,
+            color: Colors.red,
+            width: 150,
             radius: 5,
             label: 'No',
             onPressed: () {
@@ -127,74 +127,42 @@ class _LoaderTabState extends State<LoaderTab> {
           const SizedBox(
             height: 10,
           ),
-          SizedBox(
-            height: 230,
-            width: 500,
-            child: Scrollbar(
-              controller: scrollController,
-              child: ListView.builder(
-                controller: scrollController,
-                itemCount: vehicles.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  bool isSelected = selectedIndex ==
-                      index; // Check if the current item is selected
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedIndex = index; // Update the selected index
-                        vehicle = vehicles[index]['name']!;
-                      });
-                    },
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Container(
-                        height: 190,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          color: isSelected
-                              ? Colors.blue
-                              : Colors
-                                  .white, // Apply different color if selected
-                          border: Border.all(color: primary),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 10, 20, 0),
-                              child: Image.asset(
-                                'assets/images/${vehicles[index]['image']}',
-                                height: 150,
-                              ),
-                            ),
-                            const Expanded(child: SizedBox()),
-                            TextBold(
-                              text: vehicles[index]['name']!,
-                              fontSize: 14,
-                              color: isSelected
-                                  ? Colors.white
-                                  : Colors
-                                      .black, // Apply different text color if selected
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                          ],
+          Container(
+            width: 300,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: DropdownButton(
+                underline: const SizedBox(),
+                value: dropValue2,
+                items: [
+                  for (int i = 0; i < vehicles.length; i++)
+                    DropdownMenuItem(
+                      value: i,
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = i;
+
+                          vehicle = vehicles[i]['name']!;
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        child: TextRegular(
+                          text: vehicles[i]['name'].toString(),
+                          fontSize: 14,
+                          color: Colors.black,
                         ),
                       ),
                     ),
-                  );
-                },
-              ),
-            ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    dropValue2 = int.parse(value.toString());
+                  });
+                }),
           ),
           const SizedBox(
             height: 10,
